7fbf5e8689cad2e7082ed11c30be7be21aa7317c
Jose Lara <jose@hotmail.com>
2022-12-20 15:23:53 -0300

    INITIAL COMMIT

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_empty()
+    {
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee('No hay etiquetas');
+    }
+
+    public function test_with_data()
+    {
+        $tag = Tag::factory()->create();
+
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee($tag->name)
+            ->assertDontSee('No hay etiquetas');
+    }
+}
